feeece1b00580b0208dc7670e2fbc83e4fbbff8b
ALexey <Shaht3r@gmail.com>
2020-08-22 18:25:17 +0300

    feat: add endeavor form

---
+++ front/script.js
@@ -0,0 +1,28 @@
+var endeavors = [
+  {
+    name: 'Разбогатеть',
+    cats: '136'
+  }
+];
+
+function selectByButton(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  event.target.classList.toggle('selected');
+  const option = addEndeavorForm.querySelector(`option[value="${event.target.innerText}"]`);
+  option.selected = !option.selected;
+}
+
+function saveEndeavor() {
+  event.preventDefault();
+  const cats = [...addEndeavorForm.cats.options]
+    .map((option, index) => [option, index])
+    .filter(([option]) => option.selected)
+    .map(([_, index]) => index)
+    .join('');
+  endeavors.push({name: addEndeavorForm.name.value, cats});
+  console.log(endeavors);
+}
